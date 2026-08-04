4eaa7414bbd28fb049b1675ebcb84006c8e8c717
NuriOnline <dm623623@gmail.com>
2026-08-04 16:15:45 +0900

    feat: 라이브UI 추가
    라이브매니저에 있던 UI 관련 코드를 라이브UI.cs로 옮겼습니다.

--- Assets/02_Scripts/06_Live/LiveManager.cs
+++ Assets/02_Scripts/06_Live/LiveManager.cs
@@ -1,13 +1,9 @@
 using System;
-using TMPro;
 using UnityEngine;
 
 public class LiveManager : MonoBehaviour
 {
     [SerializeField] private float _liveDuration = 20f;
-
-    [SerializeField] private TMP_Text _liveStatusText;
-    [SerializeField] private TMP_Text _liveTimerText;
 
     private float _currentTime;
     private bool _isLive;
@@ -15,7 +11,6 @@ public class LiveManager : MonoBehaviour
     public bool IsLive => _isLive;
     public float CurrentTime => _currentTime;
 
-    // 라이브 시작-종료, 시간 변경을 UI, 채팅 등에 알리는 이벤트
     public event Action OnLiveStarted;
     public event Action OnLiveEnded;
     public event Action<float> OnLiveTimeChanged;
@@ -45,8 +40,6 @@ public class LiveManager : MonoBehaviour
         _isLive = true;
         _currentTime = _liveDuration;
 
-        _liveStatusText.text = "LIVE ON";
-
         Debug.Log("라이브 시작!");
 
         OnLiveStarted?.Invoke();
@@ -62,15 +55,11 @@ public class LiveManager : MonoBehaviour
 
         _isLive = false;
 
-        _liveStatusText.text = "LIVE OFF";
-        _liveTimerText.text = "00";
-
         Debug.Log("라이브 종료");
 
         OnLiveEnded?.Invoke();
     }
 
-    // 타이머 감소와 종료 처리 한 곳에 관리
     private void UpdateLiveTimer()
     {
         _currentTime -= Time.deltaTime;
--- Assets/02_Scripts/06_Live/LiveUI.cs
+++ Assets/02_Scripts/06_Live/LiveUI.cs
@@ -1,16 +1,58 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LiveUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private LiveManager _liveManager;
+
+    [SerializeField] private Button _startButton;
+
+    [SerializeField] private TMP_Text _liveStatusText;
+    [SerializeField] private TMP_Text _liveTimerText;
+
+    private void Awake()
     {
-        
+        _liveStatusText.text = "LIVE OFF";
+        _liveTimerText.text = FormatTime(_liveManager.CurrentTime);
+
+        _startButton.onClick.AddListener(_liveManager.StartLive);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        _liveManager.OnLiveStarted += ShowLiveStarted;
+        _liveManager.OnLiveEnded += ShowLiveEnded;
+        _liveManager.OnLiveTimeChanged += UpdateTimer;
+    }
+
+    private void OnDisable()
+    {
+        _liveManager.OnLiveStarted -= ShowLiveStarted;
+        _liveManager.OnLiveEnded -= ShowLiveEnded;
+        _liveManager.OnLiveTimeChanged -= UpdateTimer;
+    }
+
+    private void ShowLiveStarted()
+    {
+        _liveStatusText.text = "LIVE ON";
+    }
+
+    private void ShowLiveEnded()
+    {
+        _liveStatusText.text = "LIVE OFF";
+        _liveTimerText.text = "00:00";
+    }
+
+    private void UpdateTimer(float currentTime)
+    {
+        _liveTimerText.text = FormatTime(currentTime);
+    }
+
+    private string FormatTime(float time)
+    {
+        int seconds = Mathf.Max(0, Mathf.CeilToInt(time));
+
+        return $"00:{seconds:00}";
     }
 }
