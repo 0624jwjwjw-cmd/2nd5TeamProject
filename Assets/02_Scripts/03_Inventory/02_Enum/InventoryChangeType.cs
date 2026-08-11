90965a88a16dbb98c81fe12fa631682cc7e1764b
ma-cilg <cilg0208@gmail.com>
2026-08-11 15:14:32 +0900

    add InventroyUIController
    인벤토리 데이터와 화면 UI 사이의 중간 관리를 해주는 핵심 컨트롤러 스크립트 작성
    1. InventoryManager의 데이터를 가져오고
    2. GameDataRepository에서 이름을 찾고
    3. ItemVisualRepository에서 이미지를 찾고
    4. InventorySlotUI에 전달하는 역할 수행

---
+++ Assets/02_Scripts/03_Inventory/02_Enum/InventoryChangeType.cs
@@ -0,0 +1,9 @@
+//**인벤토리에서 어떤 종류의 변경이 발생했는지 구분하는 열거형**
+public enum InventoryChangeType
+{
+    Added,              //새로운 종류의 아이템이 인벤토리에 추가됨
+    AmountChanged,      //이미 존재하는 아이템의 수량만 변경됨
+    Removed,            //아이템 수량이 0이 되어 슬롯 자체가 제거됨
+    Sorted,             //인벤토리 슬롯의 정렬 순서가 변경됨
+    Cleared             //인벤토리 전체가 비워짐
+}
