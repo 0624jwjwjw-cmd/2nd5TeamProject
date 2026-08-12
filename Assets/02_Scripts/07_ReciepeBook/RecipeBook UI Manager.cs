88c02259d0c07017be408d51d60c74e0daf1fbd7
KimTaeYun <yuntaeyun123@gmail.com>
2026-08-12 15:27:06 +0900

    도감 재 오픈시 이전위치 저장이 아닌 맨위로 보이게

--- Assets/02_Scripts/07_ReciepeBook/RecipeBook UI Manager.cs
+++ Assets/02_Scripts/07_ReciepeBook/RecipeBook UI Manager.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RecipeBookUIManager : MonoBehaviour
 {
     [SerializeField] private GameObject recipeBook;
+    [SerializeField] private ScrollRect dishScrollRect;
 
     public void OnClickRecipeBookButton()
     {
         recipeBook.SetActive(true);
+        dishScrollRect.verticalNormalizedPosition = 1f;
+        
     }
     public void OnClickExitButton()
     {
