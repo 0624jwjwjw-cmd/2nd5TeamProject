2755a1222bf102e5f1aade3c0a6e859562faca69
재운재운 <0624jwjwjw@gmail.com>
2026-08-21 18:45:51 +0900

    후원금받기 미니게임완성
    feat.미니게임매니저
    feat.후원금 받기 게임

---
+++ Assets/02_Scripts/10_MiniGame/Coin/PlayerCoinCllecter.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class PlayerCoinCollector : MonoBehaviour
+{
+    [SerializeField] private CoinGameManager gameManager;
+    [SerializeField] private CoinText resultTextPrefab;
+    [SerializeField] private Transform resultTextRoot;
+    [SerializeField] private Vector2 resultTextOffset = new Vector2(0f, 100f);
+    private RectTransform playerRect;
+
+    private void Awake()
+    {
+        playerRect = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        Coin[] coins = FindObjectsByType<Coin>(FindObjectsSortMode.None);
+
+        foreach (Coin coin in coins)
+        {
+            if (!coin.gameObject.activeSelf)
+                continue;
+
+            RectTransform coinRect = coin.GetComponent<RectTransform>();
+
+            if (RectTransformUtility.RectangleContainsScreenPoint(playerRect,coinRect.position))
+            {
+                CollectCoin(coin);
+            }
+        }
+    }
+    private void ShowResultText(int value)
+    {
+        CoinText resultText =
+         Instantiate(resultTextPrefab, resultTextRoot);
+
+        RectTransform resultRect =
+            resultText.GetComponent<RectTransform>();
+
+        RectTransform playerRect =
+            GetComponent<RectTransform>();
+
+        resultRect.anchoredPosition =
+            playerRect.anchoredPosition + resultTextOffset;
+
+        resultText.Show(value);
+    }
+    private void CollectCoin(Coin coin)
+    {
+        int value = coin.GetCoinValue();
+        ShowResultText(value);
+        coin.Release();
+        gameManager.AddCoin(value);
+    }
+}
