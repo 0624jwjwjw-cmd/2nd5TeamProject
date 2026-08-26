38c34312a3978bf355a04f8c0cfffa399bc29ff8
ma-cilg <cilg0208@gmail.com>
2026-08-26 16:11:49 +0900

    fix GameDataRepository
    - 기존에는 ID를 이용한 단일 데이터 조회 기능만 제공
    - 요리 데이터만 순회할 수 있도록 프로퍼티 추가후 주석처리
    - 기존 private해뒀던 검색 Dictionary public으로 변경

--- Assets/02_Scripts/02_Data/00_Common/02_Interface/IGameDataRepository.cs
+++ Assets/02_Scripts/02_Data/00_Common/02_Interface/IGameDataRepository.cs
@@ -1,6 +1,8 @@
 //**게임 데이터 검색 기능의 공통 규칙을 정의하는 인터페이스**
 //
 //역할: 어떤 검색 기능이 있어야 하는지 규칙만 정의
+using System.Collections.Generic;
+
 public interface IGameDataRepository
 {
     //Repository가 정상적으로 초기화되었는지 외부에서 확인할 수 있게
@@ -10,6 +12,9 @@ public interface IGameDataRepository
     //검색 성공: ingredientData에 검색 결과 저장(true)
     //검색 실패: ingredientData는 null(false)
     bool TryGetIngredient(string itemId, out IngredientData ingredientData);
+
+    //GameDataRepository에 등록된 요리 데이터만 순회할 수 있도록 제공
+    //IEnumerable<DishData> Dishes { get; }
 
     //전달받은 일반 요리 ID와 일치하는 DishData 검색
     bool TryGetDish(string itemId, out DishData dishData);
