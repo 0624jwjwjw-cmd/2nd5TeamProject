4a8394be59782f9c7d2c1895c3ffa1e815badcb9
KimTaeYun <yuntaeyun123@gmail.com>
2026-08-28 16:31:30 +0900

    주방 작업완

--- Assets/02_Scripts/07_ReciepeBook/RecipeBook UI Manager.cs
+++ Assets/02_Scripts/07_ReciepeBook/RecipeBook UI Manager.cs
@@ -5,15 +5,16 @@ public class RecipeBookUIManager : MonoBehaviour
 {
     [SerializeField] private GameObject recipeBook;
     [SerializeField] private ScrollRect dishScrollRect;
-
+    [SerializeField] private Image background;
     public void OnClickRecipeBookButton()
     {
         recipeBook.SetActive(true);
         dishScrollRect.verticalNormalizedPosition = 1f;
-        
+        background.gameObject.SetActive(true);
     }
     public void OnClickExitButton()
     {
         recipeBook.SetActive(false);
+        background.gameObject.SetActive(false);
     }
 }
