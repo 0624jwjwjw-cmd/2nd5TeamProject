23bf6244c003f84d1e368c40ef8aa53c1999e2ce
재운재운 <0624jwjwjw@gmail.com>
2026-08-31 10:16:54 +0900

    두트윈 추가
    타이틀 거실 추가

---
+++ Assets/02_Scripts/09_Utils/DGTween/MainSean/Charater.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class CharacterTitleTween : MonoBehaviour
+{
+    [Header("Timing")]
+    [SerializeField] private float duration = 0.3f;
+
+    [Header("Scale")]
+    [SerializeField] private float firstScale = 1.2f;
+    [SerializeField] private float bounceMinScale = 0.9f;
+    [SerializeField] private float bounceMaxScale = 1.1f;
+    [SerializeField] private float finalScale = 1.2f;
+
+    [Header("Bounce")]
+    [SerializeField] private int bounceCount = 3;
+
+    public Tween Play()
+    {
+        transform.DOKill();
+
+        Sequence sequence = DOTween.Sequence();
+
+        // 0 ¡æ 1.2
+        sequence.Append(transform.DOScale(firstScale, duration).SetEase(Ease.OutBack));
+        sequence.Append(transform.DOScale(bounceMinScale, duration));
+        // 0.9 ¡ê 1.1 ¹Ýº¹
+        for (int i = 0; i < bounceCount; i++)
+        {
+            sequence.Append(transform.DOScale(bounceMaxScale, duration));
+            sequence.Append(transform.DOScale(bounceMinScale, duration));
+        }
+
+        // 1.2 ¡æ 0
+        sequence.Append(transform.DOScale(0f,duration).SetEase(Ease.InBack));
+
+        return sequence;
+    }
+}
